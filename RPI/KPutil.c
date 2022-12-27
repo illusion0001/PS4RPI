912c222a4abec3bc104385008f122e4cbf8f4547
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-12-26 21:51:05 -0600

    Update KPutil.c

--- RPI/KPutil.c
+++ RPI/KPutil.c
@@ -7,7 +7,6 @@
 void Notify(const char* FMT, ...)
 {
 	OrbisNotificationRequest Buffer;
-
 	va_list args;
 	va_start(args, FMT);
 	vsprintf(Buffer.message, FMT, args);
@@ -18,8 +17,7 @@ void Notify(const char* FMT, ...)
 	Buffer.useIconImageUri = 1;
 	Buffer.targetId = -1;
 	strcpy(Buffer.iconUri, "cxml://psnotification/tex_icon_system");
-
-	sceKernelSendNotificationRequest(0, &Buffer, 3120, 0);
+	sceKernelSendNotificationRequest(0, &Buffer, sizeof(Buffer), 0);
 }
 
 void KernelPrintOut(const char* FMT, ...)
